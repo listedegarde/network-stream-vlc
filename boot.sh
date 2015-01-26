5554d8588bb28c7a7b5feeb72d44c1ce2873ee00
Stephen <stephen@frazerfamily.ca>
2015-01-26 18:26:59 -0500

    Add colours to error messages

--- boot.sh
+++ boot.sh
@@ -1,15 +1,17 @@
+red='\033[0;31m'
+NC='\033[0m'
 if etherwake -u > /dev/null 2>&1; then
   if ping -c 1 192.168.0.10 > /dev/null 2>&1; then
     if ping -c 1 192.168.0.4 > /dev/null 2>&1; then
       etherwake 00:0a:60:5b:3a:74 &&
       etherwake namedmac;
     else
-      echo "*** WARNING - DHCP Server not running! ***";
+      echo -e "${red}*** WARNING - DHCP Server not running! ***${NC}";
     fi
   else
-    echo "*** WARNING - Server not running! ***";
+    echo -e "${red}*** WARNING - Server not running! ***${NC}";
   fi
 else
-  echo "*** WARNING - Etherwake not installed! ***"
-  echo "You can install it by typing 'apt-get install etherwake'.";
+  echo -e "${red}*** WARNING - Etherwake not installed! ***${NC}"
+  echo -e "${red}You can install it by typing 'apt-get install etherwake'.${NC}";
 fi
